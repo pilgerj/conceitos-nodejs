658a00e7e0631efebf823d214a879508472bba85
Joao Pilger <pilgerjoao@gmail.com>
2020-06-15 22:42:32 -0300

    DESAFIO 2 CONCLUIDO

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,109 @@ app.use(cors());
 
 const repositories = [];
 
+function getRepositoryIndex(id){
+  return repositories.findIndex(rep => rep.id === id);
+}
+
+function getRepository(id){
+  return repositories.find(rep => rep.id === id);
+}
+
+function hasRepository(index, response){
+  if (index > 0 ){
+    const idRep = repositories[index].id;
+    if ( getRepository(idRep) !== undefined){
+      return true;
+    }
+  }
+  return response.status(400).json({
+    message: 'Repository not found, please try another ID.'
+  })
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  const {title} = request.query;
+
+  const results = title 
+    ? repositories.filter( rep => rep.title.includes(title))
+    : repositories;
+
+  return response.status(200).json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  
+  if(title !== "" && url !== "" && techs !== []  ){
+    const newRepository = {
+      id: uuid(),
+      title,
+      url,
+      techs,
+      likes: 0
+    }
+
+    repositories.push(newRepository);
+
+    return response.status(201).json(newRepository)
+  } 
+  else {
+    return response.status(400).json({
+      message: "Error: You must complete all fields. "
+    })
+  }
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id }               = request.params;
+  const { title, url, techs} = request.body;
+  
+  const repositoryIndex = getRepositoryIndex(id);
+
+  if (hasRepository(repositoryIndex, response)){
+    const likes = repositories[repositoryIndex].likes;
+
+    const alteredRepository = {
+      id,
+      title,
+      url,
+      techs,
+      likes
+    };
+
+    repositories[repositoryIndex] = alteredRepository;
+
+    return response.json(alteredRepository);
+  }
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = getRepositoryIndex(id);
+
+  if (hasRepository(repositoryIndex, response)){
+    const title = repositories[repositoryIndex].title;
+
+    repositories.splice(repositoryIndex, 1);
+
+    return response.status(204).send()
+  } 
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+  const { id } = request.params;
+
+  const repository = getRepository(id);
+  
+  if (!repository){
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+ 
+  return response.status(200).json(repository);
+}); 
+
 
 module.exports = app;
